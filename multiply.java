c296ffd88b045f8d17479269fcbb40a9e4befa20
joanaparici <joanaparici22@gmail.com>
2022-11-27 23:38:35 +0100

    fixed first operator

--- multiply.java
+++ multiply.java
@@ -6,7 +6,7 @@ public class MultiplyTableFull {
         Scanner in = new Scanner(System.in).useLocale(Locale.US);
 
         for (int i = 1; i <= 10; i++) {
-            for (int j = 5; j <= 20; j++) {
+            for (int j = 1; j <= 20; j++) {
                 System.out.printf(" %3d", i*j);
             }            
             System.out.println();
