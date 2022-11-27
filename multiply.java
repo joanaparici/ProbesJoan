9e5e1e2a9d0e8aa98cdaa1d842f7b8138dcac43b
joanaparici <joanaparici22@gmail.com>
2022-11-27 23:20:30 +0100

    añadido muti

---
+++ multiply.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class MultiplyTableFull {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in).useLocale(Locale.US);
+
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 5; j <= 20; j++) {
+                System.out.printf(" %3d", i*j);
+            }            
+            System.out.println();
+        }
+    }
+}
